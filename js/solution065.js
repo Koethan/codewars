f774e09b03d2178ca6b68082ccde2dce7fb1bbdf
Ethan Ospina <ethan_ospina@yahoo.com>
2022-12-21 17:08:28 -0500

    run through a string and compare each character to its capital counterpart to see if its capitalized. then create a new string with all the lowercase letters and if there is a capital letter, add a space then the capital letter to split the camel case style string.

---
+++ js/solution065.js
@@ -0,0 +1,31 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+function solution(string) {
+    let arrString = string.split('')
+    let newString = ''
+      for(let i = 0; i < arrString.length; i++){
+        if(arrString[i] === arrString[i].toUpperCase()){
+
+            newString += ' '
+            newString += string[i]
+            
+    
+        }else{
+          newString += string[i]
+        }
+      }
+    return newString
+  }
+
+  console.log(solution('camelCase'))
+
+//   const solution = string => {
+//     return [...string].map((char) => {
+//       return (char === char.toUpperCase()) ? ` ${char}` : char;
+//     }).join('');
+//   }
