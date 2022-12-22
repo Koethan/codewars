90d89f78083d0143f8aebb39160ef128aa7b8071
Ethan Ospina <ethan_ospina@yahoo.com>
2022-12-22 11:00:48 -0500

    filter through array, return the length of the array after finding out how many of the elements were a match

--- js/solution065.js
+++ js/solution065.js
@@ -29,3 +29,16 @@ function solution(string) {
 //       return (char === char.toUpperCase()) ? ` ${char}` : char;
 //     }).join('');
 //   }
+
+// function solution(string) {
+//     let newString = ''
+//       for(let i = 0; i < string.length; i++){
+//         if(string[i] === string[i].toUpperCase()){
+//           newString += ' '
+//           newString += string[i]
+//         }else{
+//           newString += string[i]
+//         }
+//       }
+//     return newString
+//   }
