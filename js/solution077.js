38b7db53684c4f204d27a5620135cf071d11593e
Ethan Ospina <ethan_ospina@yahoo.com>
2022-12-27 14:45:35 -0500

    run through string thats split into an array to create a new smallest word by length then comparing to the rest of the string

---
+++ js/solution077.js
@@ -0,0 +1,21 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+    //   let wordArr = s.split('')
+    //   let newWord = wordArr.filter(word => word.length)
+    //   return Math.min(newWord)
+      let wordArr = s.split(' ')
+      let answer = wordArr[0].length
+      for(const char of wordArr){
+        if(char.length < answer){
+          answer = char.length
+        }
+      }
+      return answer
+    }
+
+    // function findShort(s){
+    //     return Math.min(...s.split(" ").map (s => s.length));
+    // }
