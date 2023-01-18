8e5f34f3479041fa1f79271166ad0121989dc498
Ethan Ospina <ethan_ospina@yahoo.com>
2023-01-18 11:31:50 -0500

    assigning a score for each letter and then comparing words in a string to see which is the most when added together

---
+++ js/solution113.js
@@ -0,0 +1,60 @@
+// Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// For example, the score of abad is 8 (1 + 2 + 1 + 4).
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+
+function high(x){
+    let objMap = {
+      a:1,
+      b:2,
+      c:3,
+      d:4,
+      e:5,
+      f:6,
+      g:7,
+      h:8,
+      i:9,
+      j:10,
+      k:11,
+      l:12,
+      m:13,
+      n:14,
+      o:15,
+      p:16,
+      q:17,
+      r:18,
+      s:19,
+      t:20,
+      u:21,
+      v:22,
+      w:23,
+      x:24,
+      y:25,
+      z:26
+    }
+    let count = 0
+    let answer = ''
+    let strArr = x.split(' ')
+    strArr.forEach((word) => {
+      let wordArr = [...word]
+      let comparisonCount = 0
+      let biggestWord = ''
+      wordArr.forEach((letter) => {
+        comparisonCount += objMap[letter]
+        biggestWord+=letter
+      })
+      if(comparisonCount > count){
+        count = comparisonCount
+        answer = biggestWord
+      }
+    })
+      return answer
+      
+    }
