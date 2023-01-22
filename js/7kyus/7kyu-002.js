2abdb73d229fd8b8e1d3bdd30f1760470fad42fc
Ethan Ospina <ethan_ospina@yahoo.com>
2023-01-21 21:06:46 -0500

    math problem, pyramid of odd numbers, return the sum of any nth row

---
+++ js/7kyus/7kyu-002.js
@@ -0,0 +1,49 @@
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+function rowSumOddNumbers(n) {
+	
+    //formula for the starting number of each row(n)
+    n*(n-1)+1
+    
+    let start = n * (n-1) + 1
+    //the array to push to, starting with the starting pyramid number
+    let answerArr = [start]
+    //the next number in the list, start it as the first number and it will be increased by 2 each loop
+    let current = start
+    
+    for(let i = 1; i < n; i++){
+      current+= 2
+      answerArr.push(current)
+    }
+    return answerArr.reduce((a,b) => a + b, 0)
+  }
+
+//   function rowSumOddNumbers(n)
+// {
+//   /* The rows' start numbers are Hogben's centered polygonal numbers:
+//      1, 3, 7, 13, 21, 31, 43 = b[n] = n^2 - n + 1.
+//      <https://oeis.org/A002061>
+     
+//      The sum of one row is given by:
+//      s[n] = n^2 + n(b[n] - 1).
+//      <https://www.quora.com/What-is-the-sum-of-n-consecutive-odd-integers/answer/Xavier-Dectot>
+     
+//      Inline b[n]:
+//      s[n] = n^2 + n(n^2 - n + 1 - 1)
+//           = n^2 + n(n^2 - n)
+//           = n^2 + n^3 - n^2
+//           = n^3
+//      ... oh. */
+//   return n * n * n;
+// }
