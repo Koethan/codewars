952fd69c69985e4aa18888d75fb160e9cc3bcb02
Ethan Ospina <ethan_ospina@yahoo.com>
2023-01-22 08:20:29 -0500

    getting the average of an array using reduce and dividing by length

--- js/8kyus/8kyu-018.js
+++ js/8kyus/8kyu-018.js
@@ -3,9 +3,12 @@
 // Note: Empty arrays should return 0.
 
 function findAverage(array) {
-    if(array === []){
+    // your code here
+    if(array.length === 0){
       return 0
     } else{
       return array.reduce((a,b)=> a + b)/ array.length
     }
+
+    return array.length === 0 ? 0 : array.reduce((a,b) => a + b, 0) / array.length
   }
